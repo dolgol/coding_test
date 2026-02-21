7b4a8a159e752295dcde6dbfc3f0a192a06673f2
dolgol <107240677+dolgol@users.noreply.github.com>
2026-02-21 18:10:55 +0900

    [level 2] Title: 주식가격, Time: 7.01 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,20 +1,14 @@
 function solution(prices) {
     let answer = [];
     for (let i = 0; i < prices.length - 1; i++) {
-        let isEnd = false;
-         // i번보다 큰 배열 prices 요소에서 i보다 작은 수 index 찾아서 빼고 answer 배열에 넣기
+        answer.push(prices.length - 1 - i);
         for (let j = i + 1; j < prices.length - 1; j++) {
             if (prices[i] > prices[j]) {
+                answer.pop();
                 answer.push(j - i);
-                isEnd = true;
                 break;
             }
         }
-        if (!isEnd) {
-            answer.push(prices.length - 1 - i);
-        }
-        // indexOf()
-        // -1이면 prices.length - 1 - i
     }
     answer.push(0);
     return answer;
