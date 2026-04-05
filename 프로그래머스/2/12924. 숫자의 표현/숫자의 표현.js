2c5341681e35df95cd16501d2ca899e2403f28cd
dolgol <107240677+dolgol@users.noreply.github.com>
2026-04-05 23:19:08 +0900

    [level 2] Title: 숫자의 표현, Time: 2.38 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,20 @@
+function solution(n) {
+    let answer = 0;
+    let start = 1;
+    
+    while (start <= n) {
+        let temp = 0;
+        for (let i = start; i < n; i++) {
+            temp += i;
+            if (temp === n) {
+                answer++;
+                break;
+            } else if (temp > n) {
+                break;
+            }
+        }
+        start++;
+    }
+    
+    return answer + 1;
+}
